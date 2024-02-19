2b966cd5102ff540473d0f156f8a187ec7c4b686
antonkornilov-ua <anton.kornilov@hotmail.com>
2024-02-19 17:32:04 +0200

    v4

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ class ECTS {
 
     static conversionTable = {
         A: [90, 100],
-        B: [80, 89],
-        C: [70, 79],
-        D: [60, 69],
-        E: [50, 59],
-        F: [0, 49],
+        B: [82, 89],
+        C: [74, 81],
+        D: [65, 73],
+        E: [60, 64],
+        F: [0, 59],
     };
 
     ectsFromScore() {
         for (const grade in ECTS.conversionTable) {
             const [min, max] = ECTS.conversionTable[grade];
-            if (this.score > min && this.score < max) {
+            if (this.score >= min && this.score <= max) {
                 return grade;
             }
         }
